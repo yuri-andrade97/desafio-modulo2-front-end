a08c3f0602fe8967330882148dc28a8d91559775
yuri-andrade97 <dev.yuri97@gmail.com>
2021-06-23 15:41:23 -0300

    ajustando css

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,7 +10,7 @@ function Navbar({ filtrarFilmes }) {
             <img src={logo} alt="" />
             <form>
                 <input type="text" placeholder="Pesquise filmes..."  onChange={event => filtrarFilmes(event.target.value) }/>
-                <button onChange={event => filtrarFilmes(event.target.value) }><img src={lupa} alt=""/></button>
+                {/* <button onChange={event => filtrarFilmes(event.target.value) }><img src={lupa} alt=""/></button> */}
             </form>
             <div className="welcome">
                 <span>Bem vindo, Yuri</span>
